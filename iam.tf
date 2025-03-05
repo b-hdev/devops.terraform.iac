022b8eff1daca4afca022a955f5a4c27c8f33090
b-hdev <bruno.bh969@gmail.com>
2025-03-04 22:42:34 -0300

    fix: role permission

--- iam.tf
+++ iam.tf
@@ -19,7 +19,7 @@ resource "aws_iam_role" "ecr_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
+        Action = "sts:AssumeRoleWithWebIdentity"
         Effect = "Allow"
         Principal = {
           Service = "ecs.amazonaws.com"
