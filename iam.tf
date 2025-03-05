9a451bb112d464c08dcfb415da265b60c4548f91
b-hdev <bruno.bh969@gmail.com>
2025-03-05 17:47:17 -0300

    new: create roles

--- iam.tf
+++ iam.tf
@@ -13,17 +13,44 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
   }
 }
 
+resource "aws_iam_role" "tf-role" {
+  name = "tf-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement : [
+      {
+        Action : "sts:AssumeRoleWithWebIdentity"
+        Condition : {
+          StringEquals : {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:b-hdev/devops.terraform.iac:ref:refs/heads/master"
+          }
+        }
+        Effect : "Allow",
+        Principal : {
+          Federated : "arn:aws:iam::741448934450:oidc-provider/token.actions.githubusercontent.com"
+          Service : "iam.amazonaws.com"
+        }
+      }
+    ]
+  })
+  tags = {
+    Iac = "True"
+  }
+}
+
 
 resource "aws_iam_role" "app-runner-role" {
   name = "app-runner-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement: [
+    Statement : [
       {
-        Effect: "Allow",
-        Principal: {
-          Service: "build.apprunner.amazonaws.com"
+        Effect : "Allow",
+        Principal : {
+          Service : "build.apprunner.amazonaws.com"
         }
         Action = "sts:AssumeRole"
       }
@@ -37,7 +64,7 @@ resource "aws_iam_role" "app-runner-role" {
 ## fix deprecated solutions in terraform
 resource "aws_iam_role_policy_attachment" "ecr_readonly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role = aws_iam_role.app-runner-role.name
+  role       = aws_iam_role.app-runner-role.name
 }
 
 ## fix deprecated solutions in terraform
@@ -53,17 +80,17 @@ resource "aws_iam_role" "ecr_role" {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
             "token.actions.githubusercontent.com:sub" = "repo:b-hdev/devops.docker.containers:ref:refs/heads/master"
-            }
+          }
         }
         Effect = "Allow"
         Principal = {
           Federated = "arn:aws:iam::741448934450:oidc-provider/token.actions.githubusercontent.com"
-          Service = "ecs.amazonaws.com"
+          Service   = "ecs.amazonaws.com"
         }
       }
     ]
   })
-    tags = {
+  tags = {
     Iac = "True"
   }
 }
@@ -77,40 +104,40 @@ resource "aws_iam_policy" "ecr_permissions_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-          Sid = "Statement1"
-          Action ="apprunner:*"
-          Effect = "Allow"
-          Resource = "*"
+        Sid      = "Statement1"
+        Action   = "apprunner:*"
+        Effect   = "Allow"
+        Resource = "*"
 
-        },
-        {
-          Sid = "Statement2"
-            Action = [
-              "iam:PassRole",
-              "iam:CreateServiceLinkedRole"
-        ]
-          Effect = "Allow"
-          Resource = "*"
-        },
-        {
-          Sid = "Statement3"
-                  Action = [
-            "ecr:GetDownloadUrlForLayer",
-            "ecr:BatchGetImage",
-            "ecr:BatchCheckLayerAvailability",
-            "ecr:PutImage",
-            "ecr:InitiateLayerUpload",
-            "ecr:UploadLayerPart",
-            "ecr:CompleteLayerUpload",
-            "ecr:GetAuthorizationToken",
+      },
+      {
+        Sid = "Statement2"
+        Action = [
+          "iam:PassRole",
+          "iam:CreateServiceLinkedRole"
         ]
         Effect   = "Allow"
         Resource = "*"
-        }
-      ]
-    })
-        tags = {
-      Iac = "True"
+      },
+      {
+        Sid = "Statement3"
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetAuthorizationToken",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+  tags = {
+    Iac = "True"
   }
 }
 
